65db9c8da549cc8160be1f845e0141ca744cf6af
Rasul1918 <darth.rasul@gmail.com>
2019-12-16 00:33:30 +0400

    added cards

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,9 +1,6 @@
 'use strict'
-import slickSlider from './slider'
 import {ableSearch, disableSearch,toggleNav} from './homepage'
 $(document).ready(function(){
-
- slickSlider();
 
  ableSearch();
  disableSearch()
